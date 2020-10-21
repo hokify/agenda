645229b6a777cb930f4231feb84ffb4e3e6dcd78
Uzlopak <aras.abbasi@googlemail.com>
2020-10-21 22:19:36 +0200

    reduce complexity of calculateProcessEvery

--- src/utils/processEvery.ts
+++ src/utils/processEvery.ts
@@ -1,16 +1,6 @@
 import * as humanInterval from 'human-interval';
-import { isValidHumanInterval } from './nextRunAt';
 
-export function calculateProcessEvery(input?: number | string): number {
-	let result = 5000;
-
-	if (typeof input === 'number') {
-		result = input;
-	}
-
-	if (isValidHumanInterval(input)) {
-		result = humanInterval(input) as number;
-	}
-
-	return result;
+export function calculateProcessEvery(input: number | string = 5000): number {
+	if (typeof input === 'number') return input;
+	return (humanInterval(input) as number) || 5000;
 }
